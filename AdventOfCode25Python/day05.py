f0786e35193a51f48564f6233c99b988ac362e1e
jasperpinkus <jasperpinkus@users.noreply.github.com>
2025-12-05 21:01:36 +0000

    day 5

--- AdventOfCode25Python/day05.py
+++ AdventOfCode25Python/day05.py
@@ -1,11 +1,45 @@
 def solve(input_path):
 
     lines = input_path.open('r').read().strip().split("\n")
-
+    lines.remove('')
     part1 = 0
     part2 = 0
 
+    freshId = []
+    avaiable = []
+
+    for line in lines:
+        if "-" in line:
+            freshId.append(list(map(int,line.split("-"))))
+        else:
+            avaiable.append(int(line))
+    avaiable.pop(0)
     
+    for item in avaiable:
+        for i in range(0, len(freshId)):
+            if freshId[i][0] <= item and freshId[i][1] >= item:
+                part1 += 1
+                break
+
+    freshId.sort()
+    idx = 0
+    while idx < len(freshId):
+        if idx+1 == len(freshId):
+            print(len(freshId))
+            print(idx)
+            break
+        elif freshId[idx][1] >= freshId[idx+1][0]:
+            if freshId[idx][1] >= freshId[idx+1][1]:
+                freshId[idx+1] = freshId[idx]
+            else: 
+                freshId[idx+1][0] = freshId[idx][0]
+            freshId.pop(idx)
+        else:
+            idx += 1
+        
+    for id in freshId:
+        part2 += id[1]-id[0]
+        part2 += 1
 
     return part1, part2
 
